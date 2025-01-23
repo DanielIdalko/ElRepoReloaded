1acf6e812470e24b31b9a53d224f6158da77c8dc
DanielIdalko <80425585+DanielIdalko@users.noreply.github.com>
2025-01-23 09:21:43 -0300

    Update BasicFieldSync.groovy
    remove comment sync

--- BasicFieldSync.groovy
+++ BasicFieldSync.groovy
@@ -11,7 +11,7 @@ class BasicFieldSync
 	issue.assignee     = nodeHelper.getUserByUsername(replica.assignee?.username)
 	issue.reporter     = nodeHelper.getUserByUsername(replica.reporter?.username)
 	issue.labels       = replica.labels
-	issue.comments     = commentHelper.mergeComments(issue, replica)
+	//issue.comments     = commentHelper.mergeComments(issue, replica)
 	//issue.attachments  = attachmentHelper.mergeAttachments(issue, replica)
   }
 }

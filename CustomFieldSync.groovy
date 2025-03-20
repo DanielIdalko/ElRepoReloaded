4a63029b548437a557a9c31aa8a899abab670deb
DanielIdalko <80425585+DanielIdalko@users.noreply.github.com>
2025-03-20 11:11:33 -0600

    Update CustomFieldSync.groovy

--- CustomFieldSync.groovy
+++ CustomFieldSync.groovy
@@ -1,5 +1,5 @@
 class CustomFieldSync {
-  static receive(issue,replica,commentsHelper){
+  static receive(issue,replica,commentHelper){
     issue.summary = replica.summary
     issue.description = replica.description
     issue.comments = commentHelper.mergeComments(issue, replica)
